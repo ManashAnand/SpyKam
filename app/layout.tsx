b01b2a83b500ef6de63f9472d4ca8d290758f555
unknown <anandmanash321@gmail.com>
2024-03-04 01:52:28 +0530

    made many different function

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,14 +19,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-          >
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+
+          <Toaster />
         </ThemeProvider>
-        </body>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,48 @@
 
 import React, { useRef, useState } from "react";
 import Webcam from "react-webcam";
-import { Separator } from "@/components/ui/separator"
+import { Separator } from "@/components/ui/separator";
 import { ModeToggle } from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
+import {
+  Camera,
+  FlipHorizontal,
+  PersonStanding,
+  Video,
+  Volume2,
+} from "lucide-react";
 
+import { toast } from "sonner";
+import { Rings } from "react-loader-spinner";
+
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { Slider } from '@/components/ui/slider';
+import { beep } from "@/utils/audio";
 
 const Home = () => {
   const webcamRef = useRef<Webcam>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   const [mirrored, setMirrored] = useState<boolean>(false);
+  const [isRecording, setIsRecording] = useState<boolean>(false);
+
+  const [autoRecordEnabled, setAutoRecordEnabled] = useState<boolean>(false);
+
+  const [volume, setVolume] = useState(0.8);
+
+  const userPromptScreenshot = () => {};
+
+  const toggleAutoRecord = () => {
+    if (autoRecordEnabled) {
+      setAutoRecordEnabled(false);
+      toast("Autorecord disabled");
+    } else {
+      setAutoRecordEnabled(true);
+      toast("Autorecord enabled");
+    }
+  };
+
+  const userPromptRecord = () => {};
 
   return (
     <div className="flex h-screen">
@@ -31,27 +64,76 @@ const Home = () => {
         <div className="border-primary/5 border-2 max-w-xs flex flex-col gap-2 justify-between shadow-md rounded-md p-4">
           {/* top secion  */}
           <div className="flex flex-col gap-2">
-            <ModeToggle/>
-            
-          <Separator className='my-2' />
-
+            <ModeToggle />
+            <Button
+              variant={"outline"}
+              size={"icon"}
+              onClick={() => {
+                setMirrored((prev) => !prev);
+              }}
+            >
+              <FlipHorizontal />
+            </Button>
+            <Separator className="my-2" />
           </div>
 
+          <div className="flex flex-col gap-2">
+            <Separator className="my-2" />
+            <Button
+              variant={"outline"}
+              size={"icon"}
+              onClick={userPromptScreenshot}
+            >
+              <Camera />
+            </Button>
+            <Separator className="my-2" />
+            <Button
+              variant={autoRecordEnabled ? "destructive" : "outline"}
+              size={"icon"}
+              onClick={toggleAutoRecord}
+            >
+              {autoRecordEnabled ? (
+                <Rings color="white" height={45} />
+              ) : (
+                <PersonStanding />
+              )}
+            </Button>
 
-          <div className='flex flex-col gap-2'>
-            <Separator className='my-2' />
-            </div>
+            <Separator className="my-2" />
+            <Button
+              variant={isRecording ? "destructive" : "outline"}
+              size={"icon"}
+              onClick={userPromptRecord}
+            >
+              <Video />
+            </Button>
+          </div>
 
-
-
-            <div className='flex flex-col gap-2'>
-            <Separator className='my-2' />  
-             </div>
-
-
-
-
+          <div className="flex flex-col gap-2">
+            <Separator className="my-2" />
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button variant={"outline"} size={"icon"}>
+                  <Volume2 />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent>
+                <Slider
+                  max={1}
+                  min={0}
+                  step={0.2}
+                  defaultValue={[volume]}
+                  onValueCommit={(val) => {
+                    setVolume(val[0]);
+                    beep(val[0]);
+                  }}
+                />
+              </PopoverContent>
+            </Popover>
+          </div>
         </div>
+
+
       </div>
     </div>
   );
