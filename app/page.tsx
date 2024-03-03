f2880bd47503c90cbfe7143ad6bc21751361eb70
unknown <anandmanash321@gmail.com>
2024-03-04 02:01:20 +0530

    made some layout changes

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,9 @@ import { Button } from "@/components/ui/button";
 import {
   Camera,
   FlipHorizontal,
+  MoonIcon,
   PersonStanding,
+  SunIcon,
   Video,
   Volume2,
 } from "lucide-react";
@@ -19,6 +21,7 @@ import { Rings } from "react-loader-spinner";
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import { Slider } from '@/components/ui/slider';
 import { beep } from "@/utils/audio";
+import SocialMediaLinks from "@/components/social-links";
 
 const Home = () => {
   const webcamRef = useRef<Webcam>(null);
@@ -30,6 +33,8 @@ const Home = () => {
   const [autoRecordEnabled, setAutoRecordEnabled] = useState<boolean>(false);
 
   const [volume, setVolume] = useState(0.8);
+  
+  const [loading, setLoading] = useState(false);
 
   const userPromptScreenshot = () => {};
 
@@ -45,7 +50,98 @@ const Home = () => {
 
   const userPromptRecord = () => {};
 
+  
+  const RenderFeatureHighlightsSection = () => {
+    return <div className="text-xs text-muted-foreground">
+      <ul className="space-y-4">
+        <li>
+          <strong>Dark Mode/Sys Theme 🌗</strong>
+          <p>Toggle between dark mode and system theme.</p>
+          <Button className="my-2 h-6 w-6" variant={"outline"} size={"icon"}>
+            <SunIcon size={14} />
+          </Button>{" "}
+          /{" "}
+          <Button className="my-2 h-6 w-6" variant={"outline"} size={"icon"}>
+            <MoonIcon size={14} />
+          </Button>
+        </li>
+        <li>
+          <strong>Horizontal Flip ↔️</strong>
+          <p>Adjust horizontal orientation.</p>
+          <Button className='h-6 w-6 my-2'
+            variant={'outline'} size={'icon'}
+            onClick={() => {
+              setMirrored((prev) => !prev)
+            }}
+          ><FlipHorizontal size={14} /></Button>
+        </li>
+        <Separator />
+        <li>
+          <strong>Take Pictures 📸</strong>
+          <p>Capture snapshots at any moment from the video feed.</p>
+          <Button
+            className='h-6 w-6 my-2'
+            variant={'outline'} size={'icon'}
+            onClick={userPromptScreenshot}
+          >
+            <Camera size={14} />
+          </Button>
+        </li>
+        <li>
+          <strong>Manual Video Recording 📽️</strong>
+          <p>Manually record video clips as needed.</p>
+          <Button className='h-6 w-6 my-2'
+            variant={isRecording ? 'destructive' : 'outline'} size={'icon'}
+            onClick={userPromptRecord}
+          >
+            <Video size={14} />
+          </Button>
+        </li>
+        <Separator />
+        <li>
+          <strong>Enable/Disable Auto Record 🚫</strong>
+          <p>
+            Option to enable/disable automatic video recording whenever
+            required.
+          </p>
+          <Button className='h-6 w-6 my-2'
+            variant={autoRecordEnabled ? 'destructive' : 'outline'}
+            size={'icon'}
+            onClick={toggleAutoRecord}
+          >
+            {autoRecordEnabled ? <Rings color='white' height={30} /> : <PersonStanding size={14} />}
+
+          </Button>
+        </li>
+
+        <li>
+          <strong>Volume Slider 🔊</strong>
+          <p>Adjust the volume level of the notifications.</p>
+        </li>
+        <li>
+          <strong>Camera Feed Highlighting 🎨</strong>
+          <p>
+            Highlights persons in{" "}
+            <span style={{ color: "#FF0F0F" }}>red</span> and other objects in{" "}
+            <span style={{ color: "#00B612" }}>green</span>.
+          </p>
+        </li>
+        <Separator />
+        <li className="space-y-4">
+          <strong>Share your thoughts 💬 </strong>
+          <SocialMediaLinks/>
+          <br />
+          <br />
+          <br />
+        </li>
+      </ul>
+    </div>
+  }
+
+
+
   return (
+    <>
     <div className="flex h-screen">
       <div className="relative">
         <div className="relative h-screen w-full">
@@ -133,9 +229,19 @@ const Home = () => {
           </div>
         </div>
 
+        <div className='h-full flex-1 py-4 px-2 overflow-y-scroll'>
+          <RenderFeatureHighlightsSection />
+        </div>
+
 
       </div>
+      {loading && 
+      <div className='z-50 absolute w-full h-full flex items-center justify-center bg-primary-foreground'>
+        Getting things ready . . . <Rings height={50} color='red' />
+      </div>}
     </div>
+    </>
+    
   );
 };
 
