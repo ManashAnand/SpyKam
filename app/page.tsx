3adbccbe1d6e2da934a182db8434362db7331413
unknown <anandmanash321@gmail.com>
2024-03-04 03:33:56 +0530

    working on tensorflow

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Webcam from "react-webcam";
 import { Separator } from "@/components/ui/separator";
 import { ModeToggle } from "@/components/ModeToggle";
@@ -18,12 +18,22 @@ import {
 import { toast } from "sonner";
 import { Rings } from "react-loader-spinner";
 
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
-import { Slider } from '@/components/ui/slider';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Slider } from "@/components/ui/slider";
 import { beep } from "@/utils/audio";
 import SocialMediaLinks from "@/components/social-links";
 
+import * as cocossd from "@tensorflow-models/coco-ssd";
+import "@tensorflow/tfjs-backend-webgl";
+import "@tensorflow/tfjs-backend-cpu";
+import { drawOnCanvas } from "@/utils/draw";
+
 const Home = () => {
+  let interval : any = null;
   const webcamRef = useRef<Webcam>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
@@ -33,8 +43,9 @@ const Home = () => {
   const [autoRecordEnabled, setAutoRecordEnabled] = useState<boolean>(false);
 
   const [volume, setVolume] = useState(0.8);
-  
+
   const [loading, setLoading] = useState(false);
+  const [model, setModel] = useState<cocossd.ObjectDetection>();
 
   const userPromptScreenshot = () => {};
 
@@ -50,198 +61,257 @@ const Home = () => {
 
   const userPromptRecord = () => {};
 
-  
-  const RenderFeatureHighlightsSection = () => {
-    return <div className="text-xs text-muted-foreground">
-      <ul className="space-y-4">
-        <li>
-          <strong>Dark Mode/Sys Theme 🌗</strong>
-          <p>Toggle between dark mode and system theme.</p>
-          <Button className="my-2 h-6 w-6" variant={"outline"} size={"icon"}>
-            <SunIcon size={14} />
-          </Button>{" "}
-          /{" "}
-          <Button className="my-2 h-6 w-6" variant={"outline"} size={"icon"}>
-            <MoonIcon size={14} />
-          </Button>
-        </li>
-        <li>
-          <strong>Horizontal Flip ↔️</strong>
-          <p>Adjust horizontal orientation.</p>
-          <Button className='h-6 w-6 my-2'
-            variant={'outline'} size={'icon'}
-            onClick={() => {
-              setMirrored((prev) => !prev)
-            }}
-          ><FlipHorizontal size={14} /></Button>
-        </li>
-        <Separator />
-        <li>
-          <strong>Take Pictures 📸</strong>
-          <p>Capture snapshots at any moment from the video feed.</p>
-          <Button
-            className='h-6 w-6 my-2'
-            variant={'outline'} size={'icon'}
-            onClick={userPromptScreenshot}
-          >
-            <Camera size={14} />
-          </Button>
-        </li>
-        <li>
-          <strong>Manual Video Recording 📽️</strong>
-          <p>Manually record video clips as needed.</p>
-          <Button className='h-6 w-6 my-2'
-            variant={isRecording ? 'destructive' : 'outline'} size={'icon'}
-            onClick={userPromptRecord}
-          >
-            <Video size={14} />
-          </Button>
-        </li>
-        <Separator />
-        <li>
-          <strong>Enable/Disable Auto Record 🚫</strong>
-          <p>
-            Option to enable/disable automatic video recording whenever
-            required.
-          </p>
-          <Button className='h-6 w-6 my-2'
-            variant={autoRecordEnabled ? 'destructive' : 'outline'}
-            size={'icon'}
-            onClick={toggleAutoRecord}
-          >
-            {autoRecordEnabled ? <Rings color='white' height={30} /> : <PersonStanding size={14} />}
+  useEffect(() => {
+    setLoading(true)
+    initModel();
+  }, []);
 
-          </Button>
-        </li>
+  const initModel = async () => {
+    const loadModel: cocossd.ObjectDetection = await cocossd.load({
+      base: "mobilenet_v2"
+    });
+    setModel(loadModel);
+  };
 
-        <li>
-          <strong>Volume Slider 🔊</strong>
-          <p>Adjust the volume level of the notifications.</p>
-        </li>
-        <li>
-          <strong>Camera Feed Highlighting 🎨</strong>
-          <p>
-            Highlights persons in{" "}
-            <span style={{ color: "#FF0F0F" }}>red</span> and other objects in{" "}
-            <span style={{ color: "#00B612" }}>green</span>.
-          </p>
-        </li>
-        <Separator />
-        <li className="space-y-4">
-          <strong>Share your thoughts 💬 </strong>
-          <SocialMediaLinks/>
-          <br />
-          <br />
-          <br />
-        </li>
-      </ul>
-    </div>
+
+  useEffect(() => {
+    if(model) setLoading(false);
+  }, [model]);
+
+
+  useEffect(() => {
+    interval = setInterval(() => {
+      runPrediction();
+    },1000)
+
+    return () => {
+      clearInterval(interval)
+    }
+  },[webcamRef.current,model])
+
+  const runPrediction = async () => {
+    if(model && webcamRef.current && webcamRef.current.video?.readyState === 4){
+      const predictions = await model.detect(webcamRef.current.video)
+
+      // console.log(predictions)
+     
+      resizeCanvas(canvasRef, webcamRef);
+      drawOnCanvas(mirrored, predictions, canvasRef.current?.getContext('2d'))
+    }
   }
 
-
-
-  return (
-    <>
-    <div className="flex h-screen">
-      <div className="relative">
-        <div className="relative h-screen w-full">
-          <Webcam
-            ref={webcamRef}
-            mirrored={mirrored}
-            className="h-full w-full object-contain p-2"
-          />
-          <canvas
-            ref={canvasRef}
-            className="absolute top-0 left-0 h-full w-full object-contain"
-          />
-        </div>
-      </div>
-      <div className="flex flex-row flex-1">
-        <div className="border-primary/5 border-2 max-w-xs flex flex-col gap-2 justify-between shadow-md rounded-md p-4">
-          {/* top secion  */}
-          <div className="flex flex-col gap-2">
-            <ModeToggle />
+  function resizeCanvas(canvasRef: React.RefObject<HTMLCanvasElement>, webcamRef: React.RefObject<Webcam>) {
+    const canvas = canvasRef.current;
+    const video = webcamRef.current?.video;
+  
+    if ((canvas && video)) {
+      const { videoWidth, videoHeight } = video;
+      canvas.width = videoWidth;
+      canvas.height = videoHeight;
+    }
+  }
+  
+  
+  const RenderFeatureHighlightsSection = () => {
+    return (
+      <div className="text-xs text-muted-foreground">
+        <ul className="space-y-4">
+          <li>
+            <strong>Dark Mode/Sys Theme 🌗</strong>
+            <p>Toggle between dark mode and system theme.</p>
+            <Button className="my-2 h-6 w-6" variant={"outline"} size={"icon"}>
+              <SunIcon size={14} />
+            </Button>{" "}
+            /{" "}
+            <Button className="my-2 h-6 w-6" variant={"outline"} size={"icon"}>
+              <MoonIcon size={14} />
+            </Button>
+          </li>
+          <li>
+            <strong>Horizontal Flip ↔️</strong>
+            <p>Adjust horizontal orientation.</p>
             <Button
+              className="h-6 w-6 my-2"
               variant={"outline"}
               size={"icon"}
               onClick={() => {
                 setMirrored((prev) => !prev);
               }}
             >
-              <FlipHorizontal />
+              <FlipHorizontal size={14} />
             </Button>
-            <Separator className="my-2" />
-          </div>
-
-          <div className="flex flex-col gap-2">
-            <Separator className="my-2" />
+          </li>
+          <Separator />
+          <li>
+            <strong>Take Pictures 📸</strong>
+            <p>Capture snapshots at any moment from the video feed.</p>
             <Button
+              className="h-6 w-6 my-2"
               variant={"outline"}
               size={"icon"}
               onClick={userPromptScreenshot}
             >
-              <Camera />
+              <Camera size={14} />
             </Button>
-            <Separator className="my-2" />
+          </li>
+          <li>
+            <strong>Manual Video Recording 📽️</strong>
+            <p>Manually record video clips as needed.</p>
             <Button
+              className="h-6 w-6 my-2"
+              variant={isRecording ? "destructive" : "outline"}
+              size={"icon"}
+              onClick={userPromptRecord}
+            >
+              <Video size={14} />
+            </Button>
+          </li>
+          <Separator />
+          <li>
+            <strong>Enable/Disable Auto Record 🚫</strong>
+            <p>
+              Option to enable/disable automatic video recording whenever
+              required.
+            </p>
+            <Button
+              className="h-6 w-6 my-2"
               variant={autoRecordEnabled ? "destructive" : "outline"}
               size={"icon"}
               onClick={toggleAutoRecord}
             >
               {autoRecordEnabled ? (
-                <Rings color="white" height={45} />
+                <Rings color="white" height={30} />
               ) : (
-                <PersonStanding />
+                <PersonStanding size={14} />
               )}
             </Button>
+          </li>
 
-            <Separator className="my-2" />
-            <Button
-              variant={isRecording ? "destructive" : "outline"}
-              size={"icon"}
-              onClick={userPromptRecord}
-            >
-              <Video />
-            </Button>
-          </div>
-
-          <div className="flex flex-col gap-2">
-            <Separator className="my-2" />
-            <Popover>
-              <PopoverTrigger asChild>
-                <Button variant={"outline"} size={"icon"}>
-                  <Volume2 />
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent>
-                <Slider
-                  max={1}
-                  min={0}
-                  step={0.2}
-                  defaultValue={[volume]}
-                  onValueCommit={(val) => {
-                    setVolume(val[0]);
-                    beep(val[0]);
-                  }}
-                />
-              </PopoverContent>
-            </Popover>
-          </div>
-        </div>
-
-        <div className='h-full flex-1 py-4 px-2 overflow-y-scroll'>
-          <RenderFeatureHighlightsSection />
-        </div>
-
-
+          <li>
+            <strong>Volume Slider 🔊</strong>
+            <p>Adjust the volume level of the notifications.</p>
+          </li>
+          <li>
+            <strong>Camera Feed Highlighting 🎨</strong>
+            <p>
+              Highlights persons in{" "}
+              <span style={{ color: "#FF0F0F" }}>red</span> and other objects in{" "}
+              <span style={{ color: "#00B612" }}>green</span>.
+            </p>
+          </li>
+          <Separator />
+          <li className="space-y-4">
+            <strong>Share your thoughts 💬 </strong>
+            <SocialMediaLinks />
+            <br />
+            <br />
+            <br />
+          </li>
+        </ul>
       </div>
-      {loading && 
-      <div className='z-50 absolute w-full h-full flex items-center justify-center bg-primary-foreground'>
-        Getting things ready . . . <Rings height={50} color='red' />
-      </div>}
-    </div>
+    );
+  };
+
+  return (
+    <>
+      <div className="flex h-screen">
+        <div className="relative">
+          <div className="relative h-screen w-full">
+            <Webcam
+              ref={webcamRef}
+              mirrored={mirrored}
+              className="h-full w-full object-contain p-2"
+            />
+            <canvas
+              ref={canvasRef}
+              className="absolute top-0 left-0 h-full w-full object-contain"
+            />
+          </div>
+        </div>
+        <div className="flex flex-row flex-1">
+          <div className="border-primary/5 border-2 max-w-xs flex flex-col gap-2 justify-between shadow-md rounded-md p-4">
+            {/* top secion  */}
+            <div className="flex flex-col gap-2">
+              <ModeToggle />
+              <Button
+                variant={"outline"}
+                size={"icon"}
+                onClick={() => {
+                  setMirrored((prev) => !prev);
+                }}
+              >
+                <FlipHorizontal />
+              </Button>
+              <Separator className="my-2" />
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <Separator className="my-2" />
+              <Button
+                variant={"outline"}
+                size={"icon"}
+                onClick={userPromptScreenshot}
+              >
+                <Camera />
+              </Button>
+              <Separator className="my-2" />
+              <Button
+                variant={autoRecordEnabled ? "destructive" : "outline"}
+                size={"icon"}
+                onClick={toggleAutoRecord}
+              >
+                {autoRecordEnabled ? (
+                  <Rings color="white" height={45} />
+                ) : (
+                  <PersonStanding />
+                )}
+              </Button>
+
+              <Separator className="my-2" />
+              <Button
+                variant={isRecording ? "destructive" : "outline"}
+                size={"icon"}
+                onClick={userPromptRecord}
+              >
+                <Video />
+              </Button>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <Separator className="my-2" />
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Button variant={"outline"} size={"icon"}>
+                    <Volume2 />
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent>
+                  <Slider
+                    max={1}
+                    min={0}
+                    step={0.2}
+                    defaultValue={[volume]}
+                    onValueCommit={(val) => {
+                      setVolume(val[0]);
+                      beep(val[0]);
+                    }}
+                  />
+                </PopoverContent>
+              </Popover>
+            </div>
+          </div>
+
+          <div className="h-full flex-1 py-4 px-2 overflow-y-scroll">
+            <RenderFeatureHighlightsSection />
+          </div>
+        </div>
+        {loading && (
+          <div className="z-50 absolute w-full h-full flex items-center justify-center bg-primary-foreground">
+            Getting things ready . . . <Rings height={50} color="red" />
+          </div>
+        )}
+      </div>
     </>
-    
   );
 };
 
